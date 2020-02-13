a350276987dcb3cb7af7872d11a68dd8c3add091
Carlos Eduardo <59carloseduardo@gmail.com>
2020-02-13 10:01:38 -0300

    Alterando Rotas
    Alterando Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoryListComponent } from './pages/categories/category-list/category-list.component';
+import { CategoryFormComponent } from './pages/categories/category-form/category-form.component';
 
 
 const routes: Routes = [
-  { path: 'categories', 
-  component: CategoryListComponent, children:[{
-    path:'',
-    loadChildren: './pages/categories/categories.module#CategoriesModule' }
+  // { path: 'categories', loadChildren: '/pages/categories/categories.module#CategoriesModule' },
 
-  ]}
+  {
+    path: '',
+    component: CategoryListComponent,
+    children: [
+      {
+        path: 'list',
+        loadChildren: './pages/categories/category-list/category-list.component'
+      }
+    ]
+  },
+  {
+    path: 'edit',
+    component: CategoryFormComponent,
+    children: [
+      {
+        path: 'form',
+        loadChildren: './pages/categories/category-list/category-form.component'
+      }
+    ]
+  }
 ];
 
 @NgModule({
